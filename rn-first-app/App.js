184fa3bfc61d945a65c247f68318161b3f6ac077
luizfverissimo <luizfverissimo@gmail.com>
2020-06-15 16:29:32 -0300

    Update App.js

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello World!");
-
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        style={styles.button}
-        title="Change Text"
-        onPress={() => setOutputText("Text changed!")}
-      />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+            width: "80%",
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  button: {
-    borderRadius: 20,
-  },
-});
+const styles = StyleSheet.create({});
