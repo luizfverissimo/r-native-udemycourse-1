6765d3ae4b54749524d10fd4adc04e25d5051f69
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-06-30 12:21:53 -0300

    shop-app started

---
+++ shop-app/constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+  primary: '#1ba681',
+  accent: '#9633d4'
+}
---
+++ shop-app/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+import { useSelector } from "react-redux";
+
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
+}
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
